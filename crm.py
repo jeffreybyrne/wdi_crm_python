e05467c7aad434071c98585a1b472f528e18ae51
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-25 14:13:26 -0500

    added a bunch of comments, finished the assignment

--- crm.py
+++ crm.py
@@ -4,12 +4,17 @@ from contact import Contact
 class CRM:
 
     def main_menu(self):
+        """This method initializes our program, making sure that the user can
+        choose an action and then do another one after that action is complete.
+        """
         while True:
             self.print_main_menu()
             selection = int(input())
             self.call_option(selection)
 
     def print_main_menu(self):
+        """This method prints out the main menu for the user
+        """
         print('[1] Add a new contact')
         print('[2] Modify an existing contact')
         print('[3] Delete a contact')
@@ -19,6 +24,9 @@ class CRM:
         print('Enter a number: ')
 
     def call_option(self, input_number):
+        """This method takes in a numeric input from the user and calls the
+        corresponding method as per the listed options from the menu.
+        """
         if input_number == 1:
             self.add_new_contact()
         elif input_number == 2:
@@ -33,6 +41,9 @@ class CRM:
             quit()
 
     def add_new_contact(self):
+        """This method prompts the user for input and creates a new contact
+        based off of their input.
+        """
         print("What is the first name of the new contact?")
         new_first_name = input()
         print("What is the last name of the new contact?")
@@ -41,9 +52,14 @@ class CRM:
         new_email = input()
         print("Enter a note for this contact:")
         new_note = input()
-        Contact.create(new_first_name, new_last_name, new_email, new_note)
+        new_contact = Contact.create(new_first_name, new_last_name, new_email, new_note)
+        print("A new contact has been added.\n{}".format(new_contact))
 
     def modify_existing_contact(self):
+        """This method prompts the user to enter a contact number to update
+        then asks which field they want to modify, what they want to change it
+        to, and then makes that change.
+        """
         print("Which contact do you want to modify?")
         search_id = int(input())
         print("What field do you wish to change?")
@@ -67,6 +83,9 @@ class CRM:
         print(contact_to_update)
 
     def delete_contact(self):
+        """This method asks the user which contact they want to delete, and
+        then deletes it.
+        """
         print("Which contact do you want to delete?")
         delete_contact = int(input())
         curr_contact = Contact.find(delete_contact)
@@ -74,20 +93,36 @@ class CRM:
         print("This contact has been deleted.")
 
     def display_all_contacts(self):
+        """This method lists all contacts.
+        """
         Contact.all()
 
     def search_by_attribute(self):
+        """This method asks the user which field they wish to search by, then
+        asks which value they want to search by, and then does a search for the
+        first contact that matches their query.
+        """
         print("What attribute would you like to search by?")
-        search_field = input()
-        while search_field not in ['first_name', 'last_name', 'email', 'note']:
-            print("Please enter a valid field to change.")
-            search_field = input()
+        print("[1] First Name")
+        print("[2] Last Name")
+        print("[3] Email Address")
+        print("[4] Notes")
+        search_field = int(input())
         print("What value would you like to search by?")
         search_value = input()
-        result = Contact.find_by(search_field, search_value)
+        if search_field == 1:
+            result = Contact.find_by('first_name', search_value)
+        elif search_field == 2:
+            result = Contact.find_by('last_name', search_value)
+        elif search_field == 3:
+            result = Contact.find_by('email', search_value)
+        elif search_field == 4:
+            result = Contact.find_by('note', search_value)
         if result is not False:
             print("The following contact matches your search criteria:")
             print(result)
+        else:
+            print("There are no contacts that match your query.")
 
 
 a_crm_app = CRM()
