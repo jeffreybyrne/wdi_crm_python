e05b2f75059f64b222c3861f9726e9e643d7016e
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-25 11:26:37 -0500

    commented out the tests at the bottom of contact, started adding functionality to crm

--- crm.py
+++ crm.py
@@ -1,25 +1,35 @@
 from contact import Contact
 
+
 class CRM:
 
-  # def main_menu(self):
-  #
-  #
-  # def print_main_menu(self):
-  #
-  #
-  # def call_option(self):
-  #
-  #
-  # def add_new_contact(self):
-  #
-  #
-  # def modify_existing_contact(self):
-  #
-  #
-  # def delete_contact(self):
-  #
-  #
-  # def display_all_contacts(self):
-  #
-  # def search_by_attribute(self):
+    running = True
+
+    def main_menu(self):
+        while CRM.running:
+            self.print_main_menu()
+            selection = int(input())
+            self.call_option(selection)
+
+    def print_main_menu(self):
+        print('Option 1')
+
+# def call_option(self):
+#
+#
+# def add_new_contact(self):
+#
+#
+# def modify_existing_contact(self):
+#
+#
+# def delete_contact(self):
+#
+#
+# def display_all_contacts(self):
+#
+# def search_by_attribute(self):
+
+
+thing = CRM()
+thing.main_menu()
