f40f88b7a563fc1f033f860bf92e693100806de1
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-26 13:36:33 -0500

    updated gitignore, started turning contact into a database

--- contact.py
+++ contact.py
@@ -1,105 +1,21 @@
-class Contact:
+from peewee import SqliteDatabase, Model, CharField, TextField
 
-    contacts = []
-    next_id = 1
+db = SqliteDatabase('crm.db')
 
-    def __init__(self, new_f_name, new_l_name, new_email, new_note):
-        """This method should initialize the contact's attributes"""
-        self.first_name = new_f_name
-        self.last_name = new_l_name
-        self.email = new_email
-        self.note = new_note
-        self.id = Contact.next_id
-        Contact.next_id += 1
 
-    @classmethod
-    def create(cls, new_f_name, new_l_name, new_email, new_note):
-        """This method should call the initializer,
-        store the newly created contact, and then return it
-        """
-        new_contact = Contact(new_f_name, new_l_name, new_email, new_note)
-        Contact.contacts.append(new_contact)
-        return new_contact
+class Contact(Model):
+    first_name = CharField()
+    last_name = CharField()
+    email = CharField()
+    notes = TextField()
 
-    @classmethod
-    def all(cls):
-        """This method should return all of the existing contacts"""
-        for num in range(0, len(cls.contacts)):
-            print(cls.contacts[num])
-        # cls.contacts = []
-
-    @classmethod
-    def find(cls, input_id):
-        """ This method should accept an id as an argument
-        and return the contact who has that id
-        """
-        for num in range(0, len(Contact.contacts)):
-            if Contact.contacts[num].id == input_id:
-                return Contact.contacts[num]
-        return False
-
-    def update(self, update_field, update_value):
-        """ This method should allow you to specify
-        1. which of the contact's attributes you want to update
-        2. the new value for that attribute
-        and then make the appropriate change to the contact
-        """
-        if update_field not in ['first_name', 'last_name', 'email', 'note']:
-            return False
-        elif update_field == 'first_name':
-            self.first_name = update_value
-            return True
-        elif update_field == 'last_name':
-            self.last_name = update_value
-            return True
-        elif update_field == 'email':
-            self.email = update_value
-            return True
-        elif update_field == 'note':
-            self.note = update_value
-            return True
-        else:
-            return False
-
-    @classmethod
-    def find_by(cls, search_field, search_value):
-        """This method should work similarly to the find method above but it
-        should allow you to search for a contact using attributes other than id
-        by specifying both the name of the attribute and the value. eg.
-        searching for 'first_name', 'Betty' should return the first contact
-        named Betty
-        """
-        for num in range(0, len(cls.contacts)):
-            curr_contact = cls.contacts[num]
-            if search_field == 'first_name' and curr_contact.first_name == search_value:
-                return curr_contact
-            elif search_field == 'last_name' and curr_contact.last_name == search_value:
-                return curr_contact
-            elif search_field == 'email' and curr_contact.email == search_value:
-                return curr_contact
-            elif search_field == 'note' and curr_contact.note == search_value:
-                return curr_contact
-            else:
-                return False
-
-    @classmethod
-    def delete_all(cls):
-        """This method should delete all of the contacts"""
-        cls.contacts = []
+    class Meta:
+        database = db
 
     def full_name(self):
         """Returns the full (first and last) name of the contact"""
-        return self.first_name + self.last_name
+        return self.first_name + ' ' + self.last_name
 
-    def delete(self):
-        """This method should delete the contact
-        HINT: Check the Array class docs for built-in methods that might be
-        useful here
-        """
-        Contact.contacts.remove(self)
-
-    def __str__(self):
-        return "#{}: {} {}, email address is {}. They have the note \"{}\"".format(self.id, self.first_name, self.last_name, self.email, self.note)
 
 # contact1 = Contact.create('Betty', 'Maker', 'bettymakes@bitmakerlabs.com', 'Loves Pokemon')
 # contact2 = Contact.create('Bit', 'Bot', 'bitbot@bitmakerlabs.com', 'beep boop')
@@ -116,3 +32,5 @@ class Contact:
 # print(Contact.all())
 # print(Contact.find_by('first_name', 'Betty'))
 # print(Contact.find_by('first_name', 'Betty2'))
+db.connect()
+db.create_tables([Contact])
